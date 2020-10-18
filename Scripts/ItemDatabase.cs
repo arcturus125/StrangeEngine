941814eca090d27b202aed414f01d713d8910ee6
arcturus125 <arcturus1259@gmail.com>
2020-10-18 15:47:39 +0100

    upload StrangeEngine V1

---
+++ Scripts/ItemDatabase.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour
+{
+    public static List<Item> itemDatabase = new List<Item>()
+    {
+        new Item
+            (
+            0,
+            "example item",
+            "this is an example item being added to the database",
+            10,
+            new Dictionary<string, int>
+            {
+                {"Sword", 100}
+            }
+            ),
+        new Item
+            (
+            1,
+            "Sword",
+            "This is a sword",
+            100,
+            new Dictionary<string, int>
+            {
+            }
+            )
+    };
+
+    public static Item SearchDatabaseByID(int ID)
+    {
+        for(int i = 0; i < itemDatabase.Count; i++)
+        {
+            if(ID == itemDatabase[i].ID)
+            {
+                return itemDatabase[i];
+            }
+        }
+        return null;
+    }
+
+    public static Item SearchDatabaseByName(string Name)
+    {
+        for(int i = 0; i< itemDatabase.Count; i++)
+        {
+            if(Name == itemDatabase[i].itemName)
+            {
+                return itemDatabase[i];
+            }
+        }
+        return null;
+    }
+}
---
+++ Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// IMPORT:: attach this script to the player
+public class Player : MonoBehaviour
+{
+    public static Inventory playerInv = new Inventory(); // create an inventory for the player
+    public static Inventory playerInvSkills = new Inventory(); // create an inventory for the player
+    public static Inventory playerInvEquips = new Inventory(); // create an inventory for the player
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        for(int i = 0; i< playerInv.inv.Count; i++)
+        {
+            //Debug.Log(playerInv.inv[i].quantity +" - "+ playerInv.inv[i].item.itemName);
+        }
+    }
+}
