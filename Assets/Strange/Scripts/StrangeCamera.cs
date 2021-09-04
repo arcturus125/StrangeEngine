feb1b5510bd7737ecf9edd32dd8a443c2299ac25
Arcturus125 <arcturus1259@gmail.com>
2021-09-04 18:23:12 +0100

    Strange Camera and Player Implemented
    use the StrangePlayer prefab as a starting point: it contains some basic gameobject hierarchy to help with scene management later and also contains a script to move the player around with the WASD keys.
    
    use the StrangeCamera prefab to look around the scene. set the target to the "Main Camera Target" on the player, and also set the player to the strange player prefab you just dragged onto the scene

---
+++ Assets/Strange/Scripts/StrangeCamera.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrangeCamera : MonoBehaviour
+{
+    public Transform target; // camera's pivot point will follow this target
+    public Transform player; // when mouse is moved along x axis, player is rotated
+
+    public float turningSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = target.position;
+
+
+
+
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        if (mouseX != 0)
+        {
+            player.Rotate(0, mouseX * turningSpeed, 0);
+            transform.RotateAround(target.position, Vector3.up, mouseX);
+        }
+        if (mouseY != 0)
+        {
+            transform.RotateAround(target.position, target.transform.right, -mouseY);
+        }
+
+
+        // basic clipping
+
+        if ( (transform.rotation.eulerAngles.x > 80.0f) && (transform.rotation.eulerAngles.x < 100.0f) )
+        {
+            transform.rotation = Quaternion.Euler(80,
+                                                  transform.rotation.eulerAngles.y,
+                                                  transform.rotation.eulerAngles.z);
+        }
+        if (transform.rotation.eulerAngles.x < 1.0f)
+        {
+            transform.rotation = Quaternion.Euler(1.0f,
+                                                  transform.rotation.eulerAngles.y,
+                                                  transform.rotation.eulerAngles.z);
+        }
+    }
+}
---
+++ Assets/Strange/Scripts/StrangePlayer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrangePlayer : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float walkSpeed = 1.0f;
+
+    // movement keys, WASD by default
+    public static KeyCode walkForward   = KeyCode.W;
+    public static KeyCode walkBack      = KeyCode.S;
+    public static KeyCode strafeLeft    = KeyCode.A;
+    public static KeyCode strafeRight   = KeyCode.D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        if (Input.GetKey(walkForward))
+        {
+            rb.AddRelativeForce(Vector3.forward * walkSpeed);
+        }
+        if (Input.GetKey(walkBack))
+        {
+            rb.AddRelativeForce(Vector3.back * walkSpeed);
+        }
+        if (Input.GetKey(strafeLeft))
+        {
+            rb.AddRelativeForce(Vector3.left * walkSpeed);
+        }
+        if (Input.GetKey(strafeRight))
+        {
+            rb.AddRelativeForce(Vector3.right * walkSpeed);
+        }
+    }
+}
