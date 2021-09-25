14129a9bec5c0d45ddcfc38591d26091a70dffed
Arcturus125 <arcturus1259@gmail.com>
2021-09-25 20:10:22 +0100

    beginning work on Enemy system
    every enemy will have an AI type.
    passive enemies will not attack the player, they will only wander around the map.
    reactive enemies will attack the player, but only if the player attacks the enemy first, otherwise they roam randomly
    Agressive enemies will attack the player as soon as they see the player. if they can not see the player, they will roam randomly

---
+++ Assets/Strange/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Enemy", menuName = "Strange/Enemy", order = 3)]
+public class Enemy : ScriptableObject
+{
+    public int health;
+    public string Name;
+
+
+    [Header("Indexes must match!")]
+    public Item[] drops;
+    public float[] dropChances;
+
+}
---
+++ Assets/Strange/Scripts/EnemyComponent.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyComponent : MonoBehaviour
+{
+    public float currentHealth;
+    public bool hit = false; // has the enemy been hit by the player?
+
+    public enum AIType
+    {
+        Passive,
+        Reactive,
+        Agressive
+    }
+    public AIType AI_type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyAI();
+    }
+
+    void EnemyAI()
+    {
+        if(AI_type == AIType.Passive)
+        {
+            // randomly roam
+        }
+        else if(AI_type == AIType.Reactive)
+        {
+            if (hit)
+            {// chase player
+            }
+            else
+            {   // randomly roam
+            }
+        }
+        else if (AI_type == AIType.Agressive)
+        {
+            // check fo LOS  to player
+            // if LOS:
+                // chase player
+            // else:
+                // randomly roam
+        }
+    }
+}
