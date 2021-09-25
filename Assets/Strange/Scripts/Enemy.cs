bf26ab06df46409c188eabc852f58c6b8e721785
Arcturus125 <arcturus1259@gmail.com>
2021-09-25 22:51:12 +0100

    Enemies now have movement
    Enemies can now wander and chase player.
    
    passive and reactive enemy types now working.
    Agressive enemy type still needs work: LOS check needs programming

--- Assets/Strange/Scripts/Enemy.cs
+++ Assets/Strange/Scripts/Enemy.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Enemy", menuName = "Strange/Enemy", order = 3)]
 public class Enemy : ScriptableObject
 {
-    public int health;
-    public string Name;
+    public int health; // the default health of the enemy when they spawn in
+    public string enemyName; // the name of the enemy
+    public float aggroRange; // the range at which the enemy will start looking for the player
+
+
+    public enum AIType
+    {
+        Passive,
+        Reactive,
+        Agressive
+    }
+    public AIType AI_type;
 
 
     [Header("Indexes must match!")]
--- Assets/Strange/Scripts/EnemyComponent.cs
+++ Assets/Strange/Scripts/EnemyComponent.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 
 public class EnemyComponent : MonoBehaviour
 {
+    public Enemy enemyReference; // the reference to the enemy file. contains the default details of the enemy
+    public GameObject Player;
+
     public float currentHealth;
     public bool hit = false; // has the enemy been hit by the player?
 
-    public enum AIType
-    {
-        Passive,
-        Reactive,
-        Agressive
-    }
-    public AIType AI_type;
 
-    // Start is called before the first frame update
+    public Enemy.AIType AI_type;
+
+
+
     void Start()
     {
-        
+        position = new Vector2(transform.position.x, transform.position.z);
+
+        //currentHealth = enemyReference.health;
+        //AI_type = enemyReference.AI_type;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
         EnemyAI();
@@ -29,20 +31,22 @@ public class EnemyComponent : MonoBehaviour
 
     void EnemyAI()
     {
-        if(AI_type == AIType.Passive)
+        if(AI_type == Enemy.AIType.Passive)
         {
-            // randomly roam
+            Wander();
         }
-        else if(AI_type == AIType.Reactive)
+        else if(AI_type == Enemy.AIType.Reactive)
         {
             if (hit)
-            {// chase player
+            {
+                ChasePlayer();
             }
             else
-            {   // randomly roam
+            {
+                Wander();
             }
         }
-        else if (AI_type == AIType.Agressive)
+        else if (AI_type == Enemy.AIType.Agressive)
         {
             // check fo LOS  to player
             // if LOS:
@@ -50,5 +54,44 @@ public class EnemyComponent : MonoBehaviour
             // else:
                 // randomly roam
         }
+    }
+
+
+    public float maxSpeed = 2;
+    public float steerStrength = 2;
+    public float wanderStrength = 0.1f;
+
+    Vector2 position;
+    Vector2 velocity;
+    Vector2 desiredDirection;
+
+    private void Wander()
+    {
+        desiredDirection = (desiredDirection + Random.insideUnitCircle * wanderStrength).normalized;
+
+        Vector2 desiredVelocity = desiredDirection * maxSpeed;
+        Vector2 desiredSteeringForce = (desiredVelocity - velocity) * steerStrength;
+        Vector2 acceleration = Vector2.ClampMagnitude(desiredSteeringForce, steerStrength) / 1;
+
+        velocity = Vector2.ClampMagnitude(velocity + acceleration * Time.deltaTime, maxSpeed);
+        position += velocity * Time.deltaTime;
+
+        float angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+        transform.SetPositionAndRotation( new Vector3(position.x, transform.position.y,position.y) , Quaternion.Euler(0, -angle, 0));
+    }
+
+    private void ChasePlayer()
+    {
+        desiredDirection = (new Vector2(Player.transform.position.x, Player.transform.position.z) - position).normalized;
+
+        Vector2 desiredVelocity = desiredDirection * maxSpeed;
+        Vector2 desiredSteeringForce = (desiredVelocity - velocity) * steerStrength;
+        Vector2 acceleration = Vector2.ClampMagnitude(desiredSteeringForce, steerStrength) / 1;
+
+        velocity = Vector2.ClampMagnitude(velocity + acceleration * Time.deltaTime, maxSpeed);
+        position += velocity * Time.deltaTime;
+
+        float angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+        transform.SetPositionAndRotation(new Vector3(position.x, transform.position.y, position.y), Quaternion.Euler(0, -angle, 0));
     }
 }
