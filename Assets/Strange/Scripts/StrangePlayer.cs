60cce6338feddafe5db60a557fc82834790f37ce
Arcturus125 <arcturus1259@gmail.com>
2021-10-04 13:37:57 +0100

    StrangePlayer now has movement options
    you can change from Movement3D (the default for 3d games)
    to Movement2D to make the enemy move in 2d games

--- Assets/Strange/Scripts/StrangePlayer.cs
+++ Assets/Strange/Scripts/StrangePlayer.cs
@@ -8,6 +8,15 @@ using UnityEngine;
 
 public class StrangePlayer : MonoBehaviour
 {
+    public enum MovementType
+    {
+        Movement3D,
+        Movement2D
+    }
+    [Tooltip("Movement3D = player moves on the X and Z plane\n\n" +
+        "Movement2D = player moves on the X and Y axis")]
+    public MovementType movementType = MovementType.Movement3D;
+
     [HideInInspector]
     public Rigidbody rb;
     public float walkSpeed = 1.0f;
@@ -21,16 +30,19 @@ public class StrangePlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+            rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        PlayerMovement();
+        if (movementType == MovementType.Movement3D)
+            PlayerMovement3D();
+        else if (movementType == MovementType.Movement2D)
+            PlayerMovement2D();
     }
 
-    private void PlayerMovement()
+    private void PlayerMovement3D()
     {
         // start with no force
         Vector3 movementForce = Vector3.zero;
@@ -49,5 +61,26 @@ public class StrangePlayer : MonoBehaviour
         Vector3 finalForce = transform.TransformDirection(movementForce.normalized * walkSpeed);
         // move the player
         rb.MovePosition(transform.position + finalForce);
+    }
+
+    private void PlayerMovement2D()
+    {
+        // start with no force
+        Vector2 movementForce = Vector2.zero;
+
+        // if buttons are pressed, accumulate force in that direction - opposite directions cancel out
+        if (Input.GetKey(walkForward))
+            movementForce += Vector2.up;
+        if (Input.GetKey(walkBack))
+            movementForce += Vector2.down;
+        if (Input.GetKey(strafeLeft))
+            movementForce += Vector2.left;
+        if (Input.GetKey(strafeRight))
+            movementForce += Vector2.right;
+
+        // normalise movementForce so that moving diagonal does not move the player faster
+        Vector2 finalForce = transform.TransformDirection(movementForce.normalized * walkSpeed);
+        // move the player
+        rb.MovePosition(transform.position + new Vector3(finalForce.x, finalForce.y));
     }
 }
