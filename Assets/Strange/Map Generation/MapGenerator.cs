bef48e5b6bd5837ac4803555a12bb4f008413e75
Arcturus125 <arcturus1259@gmail.com>
2021-10-06 21:35:37 +0100

    map generation now has colours

--- Assets/Strange/Map Generation/MapGenerator.cs
+++ Assets/Strange/Map Generation/MapGenerator.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    public enum DrawMode { NoiseMap, ColourMap}
+    public DrawMode drawMode;
+
     [Header("This script generates the map's noiseMap")]
     public int mapWidth;
     public int mapHeight;
@@ -21,12 +24,34 @@ public class MapGenerator : MonoBehaviour
 
     public bool autoUpdate;
 
+    public TerrainType[] regions;
+
     public void GenerateMap()
     {
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight,seed, noiseScale, octaves, persistance, lacunarity,offset);
+        Color[] colourMap = new Color[mapWidth * mapHeight];
+
+        for(int y = 0; y< mapHeight;y++)
+        {
+            for(int x = 0; x < mapWidth;x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for(int i = 0; i< regions.Length;i++)
+                {
+                    if(currentHeight <= regions[i].height)
+                    {
+                        colourMap[y * mapWidth + x] = regions[i].colour;
+                        break;
+                    }
+                }
+            }
+        }
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
-        display.DrawNoiseMap(noiseMap);
+        if(drawMode == DrawMode.NoiseMap)
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        else if(drawMode == DrawMode.ColourMap)
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
     }
 
     private void OnValidate()
@@ -35,6 +60,14 @@ public class MapGenerator : MonoBehaviour
         if (mapHeight < 1) mapHeight = 1;
         if (lacunarity < 1) lacunarity = 1;
         if (octaves < 0) octaves = 0;
+    }
+
+    [System.Serializable]
+    public struct TerrainType
+    {
+        public string name;
+        public float height;
+        public Color colour;
     }
 
 }
---
+++ Assets/Strange/Map Generation/TextureGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureGenerator
+{
+    public static Texture2D TextureFromColourMap(Color[] colourMap, int width, int height)
+    {
+        Texture2D texture = new Texture2D(width, height);
+        texture.filterMode = FilterMode.Point;
+        texture.wrapMode = TextureWrapMode.Clamp;
+        texture.SetPixels(colourMap);
+        texture.Apply();
+        return texture;
+    }
+    public static Texture2D TextureFromHeightMap(float[,] heightMap)
+    {
+        // get width and height from the noise map
+        int width = heightMap.GetLength(0);
+        int height = heightMap.GetLength(1);
+        //generate an array of colours
+        Color[] colorMap = new Color[width * height];
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                //generate colour between black and white where the noisemap values are used to determine colour
+                // 0 = black
+                // 0.5 = grey
+                // 1 = white
+                colorMap[y * width + x] = Color.Lerp(Color.black, Color.white, heightMap[x, y]);
+            }
+        }
+        return TextureFromColourMap(colorMap, width, height);
+    }
+}
