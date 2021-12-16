d494d78b9320166e1ce725af3aee191470ca3328
Arcturus125 <arcturus1259@gmail.com>
2021-12-16 14:04:07 +0000

    Visual scripting editor project merged into this main project

---
+++ Assets/Strange/DialogueGraph/Editor/AssetOpener.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class AssetOpener
+{
+    [OnOpenAssetAttribute(1)]
+    public static bool TestOpen(int instanceID, int line)
+    {
+        string name = EditorUtility.InstanceIDToObject(instanceID).GetType().ToString();
+        if(name == "DialogueTree")
+        {
+            string filename = EditorUtility.InstanceIDToObject(instanceID).name;
+            DialogueGraph dialogueTree =  DialogueGraph.OpenGraphWindowAndReturnInstance();
+            dialogueTree.LoadData(filename);
+
+            return true;
+        }
+        else return false;
+    }
+}
---
+++ Assets/Strange/DialogueGraph/Editor/DialogueTree.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class DialogueTree : ScriptableObject
+{
+    public NodeSaveState[] nodes;
+    public EdgeSaveState[] connections;
+}
+
+
+[Serializable]
+public class NodeSaveState
+{
+    public Rect nodePos; // position of the node
+    public bool isEntryPoint; // is this node the entrypoint?
+    public string guid; // the ID of this node
+    public string dialogueText; // the Text/Title of this node
+    public string[] choices; // choices contained within this node
+}
+
+[Serializable]
+public class EdgeSaveState
+{
+    public string inputID;
+    public string portName;
+    public string outputID;
+}
